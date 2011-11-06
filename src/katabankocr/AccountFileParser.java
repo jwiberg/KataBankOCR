79950e5dec8ec1ac766ded869788f23a380f088b
Juha Wiberg <juha.wiberg@gmail.com>
2011-11-06 21:38:23 +0200

    Second commit

--- src/katabankocr/AccountFileParser.java
+++ src/katabankocr/AccountFileParser.java
@@ -5,34 +5,38 @@ import java.util.List;
 
 public class AccountFileParser {
 
-	private String file;
+	private String[] lines;
 
 	private static final String ZERO = " _ " + "| |" + "|_|";
 	private static final String ONE = "   " + "  |" + "  |";
 	private static final String TWO = " _ " + " _|" + "|_ ";
 	private static final String THREE = " _ " + " _|" + " _|";
+	private static final String FOUR = "   " + "|_|" + "  |";
+	private static final String FIVE = " _ " + "|_ " + " _|";
+	private static final String SIX = " _ " + "|_ " + "|_|";
+	private static final String SEVEN = " _ " + "  |" + "  |";
+	private static final String EIGHT = " _ " + "|_|" + "|_|";
+	private static final String NINE = " _ " + "|_|" + " _|";
 
 	public AccountFileParser(String file) {
-		this.file = file;
+		this.lines = file.split("\n");
 	}
 
 	public List<Account> parse() {
 		List<Account> accounts = new ArrayList<Account>();
-		String[] lines = file.split("\n");
 
-		for (int i = 0; i < lines.length; i = i + 4) {			
-			String accountNumber = parseAccountNumber(lines, i);			 
+		for (int i = 0; i < lines.length; i = i + 4) {
+			String accountNumber = parseAccountNumber(lines, i);
 			accounts.add(new Account(accountNumber));
 		}
 
 		return accounts;
 	}
 
-	private String parseAccountNumber(String[] lines, int i) {
+	private String parseAccountNumber(String[] lines, int fileLineNmber) {
 		String accountNumber = "";
-		for(int j = 1; j < 10; j++) {
-			String character = getCharacter(lines, i, 1);
-			
+		for (int characterNumber = 1; characterNumber < 10; characterNumber++) {
+			String character = getCharacter(fileLineNmber, characterNumber);
 			switch (character) {
 			case ZERO:
 				accountNumber += 0;
@@ -46,24 +50,40 @@ public class AccountFileParser {
 			case THREE:
 				accountNumber += 3;
 				break;
+			case FOUR:
+				accountNumber += 4;
+				break;
+			case FIVE:
+				accountNumber += 5;
+				break;
+			case SIX:
+				accountNumber += 6;
+				break;
+			case SEVEN:
+				accountNumber += 7;
+				break;
+			case EIGHT:
+				accountNumber += 8;
+				break;
+			case NINE:
+				accountNumber += 9;
+				break;
 			default:
 				break;
-			}				
+			}
 		}
 		return accountNumber;
 	}
 
-	private String getCharacter(String[] lines, int fileLineNumber,
-			int characterNumber) {
+	private String getCharacter(int fileLineNumber, int characterNumber) {
 		String line1 = lines[fileLineNumber];
 		String line2 = lines[fileLineNumber + 1];
 		String line3 = lines[fileLineNumber + 2];
-		String character = line1.substring((characterNumber - 1) * 4,
-				(characterNumber - 1) * 4 + 3)
-				+ line2.substring((characterNumber - 1) * 4,
-						(characterNumber - 1) * 4 + 3)
-				+ line3.substring((characterNumber - 1) * 4,
-						(characterNumber - 1) * 4 + 3);
+		int startInx = (characterNumber - 1) * 3;
+		int endIndex = startInx + 3;
+		String character = line1.substring(startInx, endIndex)
+				+ line2.substring(startInx, endIndex)
+				+ line3.substring(startInx, endIndex);
 		return character;
 	}
 
--- test/katabankocr/AccountFileParserTests.java
+++ test/katabankocr/AccountFileParserTests.java
@@ -1,42 +1,109 @@
 package katabankocr;
 
+import static org.junit.Assert.assertEquals;
+
 import java.util.List;
 import org.junit.Test;
-import static org.junit.Assert.*;
 
 public class AccountFileParserTests {
 
 	@Test
 	public void parseZeros() {		 
 		String file = ZEROS;
-		AccountFileParser parser = new AccountFileParser(file);
-		List<Account> accounts = parser.parse();
+		List<Account> accounts = parseFile(file);
 		assertEquals("000000000", accounts.get(0).getAccountNumber());
 	}
 	
 	@Test
 	public void parseOnes() {		 
 		String file = ONES;
-		AccountFileParser parser = new AccountFileParser(file);
-		List<Account> accounts = parser.parse();
+		List<Account> accounts = parseFile(file);
 		assertEquals("111111111", accounts.get(0).getAccountNumber());
 	}	
 	
 	@Test
 	public void parseTwos() {
 		String file = TWOS;
-		AccountFileParser parser = new AccountFileParser(file);
-		List<Account> accounts = parser.parse();
+		List<Account> accounts = parseFile(file);
 		assertEquals("222222222", accounts.get(0).getAccountNumber());		
 	}
 	
 	@Test
 	public void parseThrees() {
 		String file = THREES;
+		List<Account> accounts = parseFile(file);
+		assertEquals("333333333", accounts.get(0).getAccountNumber());		
+	}
+	
+	@Test
+	public void parseFours() {
+		String file = FOURS;
+		List<Account> accounts = parseFile(file);
+		assertEquals("444444444", accounts.get(0).getAccountNumber());		
+	}
+	
+	@Test
+	public void parseFives() {
+		String file = FIVES;
+		List<Account> accounts = parseFile(file);
+		assertEquals("555555555", accounts.get(0).getAccountNumber());		
+	}
+	
+	@Test
+	public void parseSixs() {
+		String file = SIXS;
+		List<Account> accounts = parseFile(file);
+		assertEquals("666666666", accounts.get(0).getAccountNumber());		
+	}	
+	
+	@Test
+	public void parseSevens() {
+		String file = SEVENS;
+		List<Account> accounts = parseFile(file);
+		assertEquals("777777777", accounts.get(0).getAccountNumber());		
+	}
+	
+	@Test
+	public void parseEights() {
+		String file = EIGHTS;
+		List<Account> accounts = parseFile(file);
+		assertEquals("888888888", accounts.get(0).getAccountNumber());		
+	}
+	
+	@Test
+	public void parseNines() {
+		String file = NINES;
+		List<Account> accounts = parseFile(file);
+		assertEquals("999999999", accounts.get(0).getAccountNumber());		
+	}		
+	
+	@Test
+	public void parseOnesAndZeros() {
+		String file = ONES_AND_ZEROS;
+		List<Account> accounts = parseFile(file);
+		assertEquals("101010101", accounts.get(0).getAccountNumber());		
+	}	
+	
+	@Test
+	public void parseAllNumbers() {
+		String file = ALL_NUMBERS;
+		List<Account> accounts = parseFile(file);
+		assertEquals("123456789", accounts.get(0).getAccountNumber());
+	}
+	
+	@Test
+	public void parseTwoLines() {
+		String file = ONES_AND_ZEROS + ALL_NUMBERS;
+		List<Account> accounts = parseFile(file);
+		assertEquals("101010101", accounts.get(0).getAccountNumber());
+		assertEquals("123456789", accounts.get(1).getAccountNumber());
+	}
+
+	private List<Account> parseFile(String file) {
 		AccountFileParser parser = new AccountFileParser(file);
 		List<Account> accounts = parser.parse();
-		assertEquals("333333333", accounts.get(0).getAccountNumber());		
-	}	
+		return accounts;
+	}			
 	
 	private static final String ZEROS =   " _  _  _  _  _  _  _  _  _ \n"
 									    + "| || || || || || || || || |\n"
@@ -55,7 +122,46 @@ public class AccountFileParserTests {
 	
 	private static final String THREES =   " _  _  _  _  _  _  _  _  _ \n"
 										 + " _| _| _| _| _| _| _| _| _|\n"
-                                         + " _| _| _| _| _| _| _| _| _|\n";
+                                         + " _| _| _| _| _| _| _| _| _|\n"
+										 + "                           \n";
 			                            
+	private static final String FOURS =    "                           \n"
+										 + "|_||_||_||_||_||_||_||_||_|\n"
+							             + "  |  |  |  |  |  |  |  |  |\n"
+									     + "                           \n";
 
+	private static final String FIVES =    " _  _  _  _  _  _  _  _  _ \n"
+										 + "|_ |_ |_ |_ |_ |_ |_ |_ |_ \n"
+								         + " _| _| _| _| _| _| _| _| _|\n"
+								         + "                           \n";
+
+	private static final String SIXS =     " _  _  _  _  _  _  _  _  _ \n"
+										 + "|_ |_ |_ |_ |_ |_ |_ |_ |_ \n"
+								         + "|_||_||_||_||_||_||_||_||_|\n"
+										 + "                           \n";
+
+	private static final String SEVENS =   " _  _  _  _  _  _  _  _  _ \n"
+										 + "  |  |  |  |  |  |  |  |  |\n"
+								         + "  |  |  |  |  |  |  |  |  |\n"
+			                             + "                           \n";
+
+	private static final String EIGHTS =   " _  _  _  _  _  _  _  _  _ \n"
+										 + "|_||_||_||_||_||_||_||_||_|\n"
+								         + "|_||_||_||_||_||_||_||_||_|\n"
+										 + "                           \n";
+
+	private static final String NINES =    " _  _  _  _  _  _  _  _  _ \n"
+										 + "|_||_||_||_||_||_||_||_||_|\n"
+								         + " _| _| _| _| _| _| _| _| _|\n"
+										 + "                           \n";	
+	
+	private static final String ONES_AND_ZEROS =   "    _     _     _     _    \n"
+											     + "  || |  || |  || |  || |  |\n"
+											     + "  ||_|  ||_|  ||_|  ||_|  |\n"
+												 + "                           \n";
+	
+	private static final String ALL_NUMBERS = "    _  _     _  _  _  _  _ \n"
+											+ "  | _| _||_||_ |_   ||_||_|\n"
+											+ "  ||_  _|  | _||_|  ||_| _|\n"
+											+ "                           \n";	
 }
