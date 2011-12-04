732b38da88285151ae3cdea363a6e27699076859
Juha Wiberg <juha.wiberg@gmail.com>
2011-12-04 21:42:07 +0200

    New test for log file writing.

--- test/katabankocr/AllTests.java
+++ test/katabankocr/AllTests.java
@@ -6,7 +6,6 @@ import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
 @SuiteClasses({ AccountFileParserTests.class, AccountTests.class,
-		LogWriterTests.class })
+		LogWriterTests.class, LogFileWriterTests.class })
 public class AllTests {
-
 }
--- test_integration/katabankocr/LogFileWriterTests.java
+++ test_integration/katabankocr/LogFileWriterTests.java
@@ -1,6 +1,11 @@
 package katabankocr;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.util.ArrayList;
 import java.util.List;
@@ -9,9 +14,9 @@ import org.junit.After;
 import org.junit.Test;
 
 public class LogFileWriterTests {
-	
+
 	private static final String TEST_FILE_NAME = "accounts.dat";
-	
+
 	@After
 	public void clean() {
 		File file = new File(TEST_FILE_NAME);
@@ -19,17 +24,42 @@ public class LogFileWriterTests {
 	}
 
 	@Test
-	public void writeSimpleFile() throws Exception{
-		try(FileWriter fr = new FileWriter(TEST_FILE_NAME)) {
+	public void writeSimpleFile() throws Exception {
+		try (FileWriter fr = new FileWriter(TEST_FILE_NAME)) {
 			Account account = new Account("123456789");
 			List<Account> accounts = new ArrayList<Account>();
 			accounts.add(account);
 			LogWriter logWriter = new LogWriter();
 			logWriter.setFileWriter(fr);
-			logWriter.write(accounts);					
+			logWriter.write(accounts);
 		}
-		
+
 		File file = new File(TEST_FILE_NAME);
-		assert(file.exists());
+		assertTrue(file.exists());
+	}
+
+	@Test
+	public void write3AccountsToFile() throws Exception {
+		try (FileWriter fr = new FileWriter(TEST_FILE_NAME)) {
+			Account accountValid = new Account("123456789");
+			Account accountInvalid = new Account("132453455");
+			Account accountIllegalCharaters = new Account("13??53455");
+			List<Account> accounts = new ArrayList<Account>();
+			accounts.add(accountValid);
+			accounts.add(accountInvalid);
+			accounts.add(accountIllegalCharaters);
+			LogWriter logWriter = new LogWriter();
+			logWriter.setFileWriter(fr);
+			logWriter.write(accounts);
+		}
+
+		BufferedReader reader = new BufferedReader(new FileReader(
+				TEST_FILE_NAME));
+			
+		assertEquals("123456789", reader.readLine());
+		assertEquals("132453455 ERR", reader.readLine());
+		assertEquals("13??53455 ILL", reader.readLine());
+		
+		reader.close();
 	}
 }
