d5547b1011df8d89106647a865cf67be3622724d
Juha Wiberg <juha.wiberg@gmail.com>
2011-12-04 21:04:02 +0200

    Renaming classes. Refactoring.

--- test/katabankocr/AllTests.java
+++ test/katabankocr/AllTests.java
@@ -6,7 +6,7 @@ import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
 @SuiteClasses({ AccountFileParserTests.class, AccountTests.class,
-		LogFileWriterTests.class })
+		LogWriterTests.class })
 public class AllTests {
 
 }
